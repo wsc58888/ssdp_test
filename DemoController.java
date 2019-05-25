033a63696c1e6704466b0203492a44a213874e6b
wsc58888 <wsc58888@163.com>
2019-05-25 09:33:26 +0800

    Add files via upload

---
+++ DemoController.java
@@ -0,0 +1,38 @@
+package com.flowable.controller;
+
+import org.flowable.ui.common.model.UserRepresentation;
+import org.flowable.ui.common.security.DefaultPrivileges;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AppController
+ * @author jinlei
+ * @date 2019/3/13 15:34
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping(value = "/rest/account", method = RequestMethod.GET, produces = "application/json")
+    public UserRepresentation getAccount() {
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setId("admin");
+        userRepresentation.setEmail("admin@flowable.org");
+        userRepresentation.setFullName("Test Administrator");
+        userRepresentation.setLastName("Administrator");
+        userRepresentation.setFirstName("Test");
+        List<String> privileges = new ArrayList<>();
+        privileges.add(DefaultPrivileges.ACCESS_MODELER);
+        privileges.add(DefaultPrivileges.ACCESS_IDM);
+        privileges.add(DefaultPrivileges.ACCESS_ADMIN);
+        privileges.add(DefaultPrivileges.ACCESS_TASK);
+        privileges.add(DefaultPrivileges.ACCESS_REST_API);
+        userRepresentation.setPrivileges(privileges);
+        return userRepresentation;
+    }
+}
